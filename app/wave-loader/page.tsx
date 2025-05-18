1767e40acf1f4ca56791bba4c48c267221a60cf5
Alireza Aberoumand <maya7192@yahoo.com>
2025-05-18 09:53:39 -0700

    wave-loader added

--- app/wave-loader/page.tsx
+++ app/wave-loader/page.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const WaveLoader = () => {
   return (
-    <div>WaveLoader</div>
+    <div className='w-full h-screen flex justify-center gap-5 items-center'>
+        <div className='w-10 h-50 origin-center bg-indigo-700 rounded scale-y-20 animate-grow-1'></div>
+        <div className='w-10 h-50 origin-center bg-indigo-700 rounded scale-y-20 animate-grow-2'></div>
+        <div className='w-10 h-50 origin-center bg-indigo-700 rounded scale-y-20 animate-grow-3'></div>
+        <div className='w-10 h-50 origin-center bg-indigo-700 rounded scale-y-20 animate-grow-4'></div>
+        <div className='w-10 h-50 origin-center bg-indigo-700 rounded scale-y-20 animate-grow-5'></div>
+    </div>
   )
 }
 
